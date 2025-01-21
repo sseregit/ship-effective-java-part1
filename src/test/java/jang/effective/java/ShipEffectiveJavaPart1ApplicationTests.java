bd929f47adbc986a02b6fdf531d111029e88c08d
장연식 <sseregit@knowwheresoft.com>
2025-01-21 15:50:56 +0900

    init

---
+++ src/test/java/jang/effective/java/ShipEffectiveJavaPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jang.effective.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipEffectiveJavaPart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
