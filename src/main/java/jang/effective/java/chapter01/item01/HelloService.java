06147e2f905c2f881192ac58b5a244dac21c796e
장연식 <sseregit@knowwheresoft.com>
2025-01-22 14:26:55 +0900

    아이템 1. 완벽 공략 3 - 인터페이스와 정적 메서드

--- src/main/java/jang/effective/java/chapter01/item01/HelloService.java
+++ src/main/java/jang/effective/java/chapter01/item01/HelloService.java
@@ -3,4 +3,25 @@ package jang.effective.java.chapter01.item01;
 public interface HelloService {
 
     String hello();
+
+    default String hi1() {
+        prepareMessage();
+        return "hi";
+    }
+    default String hi2() {
+        prepareMessage();
+        return "hi";
+    }
+    default String hi3() {
+        prepareMessage();
+        return "hi";
+    }
+
+    private void prepareMessage() {
+        // ....
+    }
+
+    static String bye() {
+        return "bye";
+    }
 }
