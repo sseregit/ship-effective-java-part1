58e104a72d9f38685fbe1403c5349abcbb51cd27
장연식 <sseregit@knowwheresoft.com>
2025-01-22 14:06:24 +0900

    아이템 1. 완벽 공략 1 - 열거 타입

--- src/main/java/jang/effective/java/chapter01/item01/Order.java
+++ src/main/java/jang/effective/java/chapter01/item01/Order.java
@@ -5,6 +5,7 @@ public class Order {
     private boolean prime;
     private boolean urgent;
     private Product product;
+    private OrderStatus orderStatus;
 
     public static Order primeOrder(Product product) {
         Order order = new Order();
@@ -18,5 +19,16 @@ public class Order {
         order.urgent = true;
         order.product = product;
         return order;
+    }
+
+    public static void main(String[] args) {
+        for (OrderStatus value : OrderStatus.values()) {
+            System.out.println(value);
+        }
+
+        Order order = new Order();
+        if (order.orderStatus == OrderStatus.DELIVERED) {
+            System.out.println("delivered");
+        }
     }
 }
