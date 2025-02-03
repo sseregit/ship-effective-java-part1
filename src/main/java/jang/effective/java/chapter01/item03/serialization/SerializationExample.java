8eaa113770880ce182ef87b9b538990dac087405
장연식 <sseregit@knowwheresoft.com>
2025-02-03 16:27:43 +0900

    아이템 3. 완벽 공략 13 - 객체 직렬화

---
+++ src/main/java/jang/effective/java/chapter01/item03/serialization/SerializationExample.java
@@ -0,0 +1,35 @@
+package jang.effective.java.chapter01.item03.serialization;
+
+import java.io.*;
+
+public class SerializationExample {
+
+    private void serialize(Book book) {
+        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("book.obj"))) {
+            out.writeObject(book);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Book deserialize(Book book) {
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("book.obj"))) {
+            return (Book) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+//        Book book = new Book("12345", "이팩티브 자바 완벽 공략", "백기선", LocalDate.of(2022, 3, 21));
+//        book.setNumberOfSold(200);
+
+        SerializationExample example = new SerializationExample();
+//        example.serialize(book);
+        Book deserializedBook = example.deserialize(null);
+
+//        System.out.println(book);
+        System.out.println(deserializedBook);
+    }
+
+}
