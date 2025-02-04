36e2ed1f9b9a163b494afd494147bc786431e628
장연식 <sseregit@knowwheresoft.com>
2025-02-04 14:32:03 +0900

    아이템 6. 완벽 공략 16 - deprecation

---
+++ src/main/java/jang/effective/java/chapter01/item06/Client.java
@@ -0,0 +1,9 @@
+package jang.effective.java.chapter01.item06;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Deprecation deprecation = new Deprecation();
+        Deprecation stringDeprecation = new Deprecation("string");
+    }
+}
