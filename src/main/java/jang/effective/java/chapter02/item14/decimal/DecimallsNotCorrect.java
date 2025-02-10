c89eac15c759d0d5ea9de445f6d0dd3aa5a25187
장연식 <sseregit@knowwheresoft.com>
2025-02-10 11:42:26 +0900

    아이템 14. 완벽 공략

---
+++ src/main/java/jang/effective/java/chapter02/item14/decimal/DecimallsNotCorrect.java
@@ -0,0 +1,15 @@
+package jang.effective.java.chapter02.item14.decimal;
+
+import java.math.BigDecimal;
+
+public class DecimallsNotCorrect {
+
+    public static void main(String[] args) {
+        int i = 1;
+        double d = 0.1;
+        System.out.println(i - d * 9);
+
+        BigDecimal bd = BigDecimal.valueOf(0.1);
+        System.out.println(BigDecimal.valueOf(1).min(bd.multiply(BigDecimal.valueOf(9))));
+    }
+}
