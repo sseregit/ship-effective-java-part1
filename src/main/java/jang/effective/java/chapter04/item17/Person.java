5303c0b49fe50f12721dcb2c617c20240c2402ca
장연식 <sseregit@knowwheresoft.com>
2025-02-11 11:52:17 +0900

    아이템 17. 핵심 정리 - 불변 클래스

---
+++ src/main/java/jang/effective/java/chapter04/item17/Person.java
@@ -0,0 +1,28 @@
+package jang.effective.java.chapter04.item17;
+
+public final class Person {
+    private final Address address;
+
+    public Person(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        Address copyofAddress = new Address();
+        copyofAddress.setCity(address.getCity());
+        copyofAddress.setStreet(address.getStreet());
+        copyofAddress.setZipCode(address.getZipCode());
+        return copyofAddress;
+//        return address;
+    }
+
+    public static void main(String[] args) {
+        Address seattle = new Address();
+        seattle.setCity("Seattle");
+        Person person = new Person(seattle);
+        Address redmond = person.getAddress();
+        redmond.setCity("Redmond");
+
+        System.out.println(person.getAddress().getCity());
+    }
+}
