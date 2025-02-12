bf800e610a91d0ea37767d0e4ccdb8d01e4e1d1f
장연식 <sseregit@knowwheresoft.com>
2025-02-12 14:14:04 +0900

    아이템 22. 핵심 정리

---
+++ src/main/java/jang/effective/java/chapter04/item22/constantinterface/MyClass.java
@@ -0,0 +1,8 @@
+package jang.effective.java.chapter04.item22.constantinterface;
+
+public class MyClass implements PhysicalConstants {
+
+    public static void main(String[] args) {
+        System.out.println(BOLTZMANN_CONSTANT);
+    }
+}
---
+++ src/main/java/jang/effective/java/chapter04/item22/constantutillityclass/PhysicalConstants.java
@@ -0,0 +1,13 @@
+package jang.effective.java.chapter04.item22.constantutillityclass;
+
+public class PhysicalConstants {
+
+    private PhysicalConstants() {
+    }
+
+    public static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+
+    public static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+
+    public static final double ELECTRON_MASS = 9.109_383_56e-31;
+}
