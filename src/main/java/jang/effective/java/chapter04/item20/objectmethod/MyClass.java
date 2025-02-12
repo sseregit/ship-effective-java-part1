50531bc9f329037b83ae80b41aad4183b0c33ce3
장연식 <sseregit@knowwheresoft.com>
2025-02-12 11:42:34 +0900

    아이템 20. 완벽 공략 - 디폴트 메서드와 Object 메서드

---
+++ src/main/java/jang/effective/java/chapter04/item20/objectmethod/MyClass.java
@@ -0,0 +1,4 @@
+package jang.effective.java.chapter04.item20.objectmethod;
+
+public class MyClass extends Object { // implements MyInterface {
+}
---
+++ src/main/java/jang/effective/java/chapter04/item20/objectmethod/MyInterface.java
@@ -0,0 +1,16 @@
+package jang.effective.java.chapter04.item20.objectmethod;
+
+public class MyInterface {
+
+    /*default boolean equals(Object obj) {
+        return true;
+    }
+
+    default int hashCode() {
+        return 10;
+    }
+
+    default String toString() {
+        return "myString";
+    }*/
+}
