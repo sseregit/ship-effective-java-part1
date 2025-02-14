77c4a3d2cec75f631db2537f8af5fe5ea883cf23
장연식 <sseregit@knowwheresoft.com>
2025-02-14 17:31:40 +0900

    아이템 34. 핵심 정리 5: 열거 타입의 제약

--- src/main/java/jang/effective/java/chapter06/item34/operation/OperationOrigin.java
+++ src/main/java/jang/effective/java/chapter06/item34/operation/OperationOrigin.java
@@ -1,8 +1,17 @@
 package jang.effective.java.chapter06.item34.operation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum OperationOrigin {
 
     PLUS, MINUS, TIMES, DIVIDE, NEW_OPERATION;
+
+    private static final Map<String, OperationOrigin> stringToEnum = new HashMap<>();
+
+    OperationOrigin() {
+//        stringToEnum.put(toString(), this);
+    }
 
     public double apply(double x, double y) {
         switch (this) {
