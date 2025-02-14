7f5191e9eb3d7e9c5f9bebcc6fe565a98ecb21e9
장연식 <sseregit@knowwheresoft.com>
2025-02-14 13:38:53 +0900

    아이템 33. 핵심 정리 - 타입 토큰을 사용한 타입 안전 이종 컨테이너

---
+++ src/main/java/jang/effective/java/chapter05/item33/type_token/Favrites.java
@@ -0,0 +1,38 @@
+package jang.effective.java.chapter05.item33.type_token;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class Favrites {
+
+    private final Map<Class<?>, Object> map = new HashMap<>();
+
+    public <T> void put(Class<T> clazz, T value) {
+        map.put(Objects.requireNonNull(clazz), clazz.cast(value));
+    }
+
+    public <T> T get(Class<T> clazz) {
+        return clazz.cast(map.get(clazz));
+    }
+
+    public static void main(String[] args) {
+        Favrites favrites = new Favrites();
+//        favrites.put(String.class, 1);
+//        favrites.put((Class) String.class, 1);
+
+//        favrites.put(String.class, "1");
+        favrites.put(Integer.class, 2);
+//        favrites.put(Integer.class, "2");
+
+        Integer i = favrites.get(Integer.class);
+        String s = favrites.get(String.class);
+
+        favrites.put(List.class, List.of(1, 2, 3));
+        favrites.put(List.class, List.of("a", "b", "c"));
+
+        List list = favrites.get(List.class);
+        list.forEach(System.out::println);
+    }
+}
