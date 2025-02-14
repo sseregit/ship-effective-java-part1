e8d08a75785eee01d5da5a06639d45db7af533e2
장연식 <sseregit@knowwheresoft.com>
2025-02-14 17:39:44 +0900

    아이템 35. 핵심 정리

---
+++ src/main/java/jang/effective/java/chapter06/item35/anti_pattern/Ensemble.java
@@ -0,0 +1,10 @@
+package jang.effective.java.chapter06.item35.anti_pattern;
+
+public enum Ensemble {
+    SOLO, DUET, TRIO, QUARTET, QUINTET,
+    SEXTET, SEPTET, OCTET, NONET, DECTET;
+
+    public int numberOfMusicians() {
+        return ordinal() + 1;
+    }
+}
