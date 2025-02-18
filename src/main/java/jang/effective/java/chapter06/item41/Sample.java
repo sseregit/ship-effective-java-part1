ac66237c7ade6f545ab93c9e61bca80c65dad13a
장연식 <sseregit@knowwheresoft.com>
2025-02-18 18:08:51 +0900

    아이템 41. 핵심 정리

---
+++ src/main/java/jang/effective/java/chapter06/item41/Sample.java
@@ -0,0 +1,7 @@
+package jang.effective.java.chapter06.item41;
+
+import java.io.Serializable;
+
+public class Sample implements Serializable {
+
+}
---
+++ src/main/java/jang/effective/java/chapter06/item41/marker_interface/MyInterface.java
@@ -0,0 +1,4 @@
+package jang.effective.java.chapter06.item41.marker_interface;
+
+public interface MyInterface {
+}
